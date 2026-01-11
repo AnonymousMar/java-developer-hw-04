a84a765aa7b3827e0a2b33dfa873333926edbfaa
Maryna <119819802+AnonymousMar@users.noreply.github.com>
2026-01-11 20:59:48 +0100

    Add files via upload

--- find_longest_project.sql
+++ find_longest_project.sql
@@ -1,37 +1,4 @@
-/* create row with projects names and find the longest project */
-ALTER TABLE project ADD COLUMN name VARCHAR(25);
-
-UPDATE project
-SET name = 'Project A'
-WHERE id = 1;
-UPDATE project
-SET name = 'Project B'
-WHERE id = 2;
-UPDATE project
-SET name = 'Project C'
-WHERE id = 3;
-UPDATE project
-SET name = 'Project D'
-WHERE id = 4;
-UPDATE project
-SET name = 'Project E'
-WHERE id = 5;
-UPDATE project
-SET name = 'Project F'
-WHERE id = 6;
-UPDATE project
-SET name = 'Project G'
-WHERE id = 7;
-UPDATE project
-SET name = 'Project H'
-WHERE id = 8;
-UPDATE project
-SET name = 'Project I'
-WHERE id = 9;
-UPDATE project
-SET name = 'Project J'
-WHERE id = 10;
-
+/*find the longest project */
 SELECT name, DATEDIFF('MONTH', start_date, finish_date) AS month_count
 FROM project
 WHERE DATEDIFF('MONTH', start_date, finish_date) = (
--- init_db.sql
+++ init_db.sql
@@ -27,6 +27,40 @@ client_id INT,
 start_date DATE,
 finish_date DATE
 );
+
+ALTER TABLE project ADD COLUMN name VARCHAR(25);
+
+UPDATE project
+SET name = 'Project A'
+WHERE id = 1;
+UPDATE project
+SET name = 'Project B'
+WHERE id = 2;
+UPDATE project
+SET name = 'Project C'
+WHERE id = 3;
+UPDATE project
+SET name = 'Project D'
+WHERE id = 4;
+UPDATE project
+SET name = 'Project E'
+WHERE id = 5;
+UPDATE project
+SET name = 'Project F'
+WHERE id = 6;
+UPDATE project
+SET name = 'Project G'
+WHERE id = 7;
+UPDATE project
+SET name = 'Project H'
+WHERE id = 8;
+UPDATE project
+SET name = 'Project I'
+WHERE id = 9;
+UPDATE project
+SET name = 'Project J'
+WHERE id = 10;
+
 /*table project_worker*/
 CREATE TABLE project_worker(
 project_id INT NOT NULL,
