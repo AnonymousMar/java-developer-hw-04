62cea3f3d78b93f08a2e985feb105f3d10ec4e14
Maryna <119819802+AnonymousMar@users.noreply.github.com>
2026-01-12 13:59:59 +0100

    Add files via upload

--- init_db.sql
+++ init_db.sql
@@ -9,11 +9,9 @@ salary INT,
 CHECK(CHAR_LENGTH(name) BETWEEN 2 AND 1000),
 CHECK(YEAR(birthday) > 1900),
 CHECK(salary > 100 AND salary < 10000)
+CHECK(level IN ('Trainee', 'Junior', 'Middle', 'Senior'));
 );
 
-ALTER TABLE worker
-ADD CONSTRAINT level_values
-CHECK(level IN ('Trainee', 'Junior', 'Middle', 'Senior'));
 /*table client*/
 CREATE TABLE client(
 id INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,43 +21,12 @@ CHECK(LENGTH(name) BETWEEN 2 AND 1000)
 /*table project*/
 CREATE TABLE project(
 id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(25);
 client_id INT,
 start_date DATE,
-finish_date DATE
+finish_date DATE,
 );
 
-ALTER TABLE project ADD COLUMN name VARCHAR(25);
-
-UPDATE project
-SET name = 'Project A'
-WHERE id = 1;
-UPDATE project
-SET name = 'Project B'
-WHERE id = 2;
-UPDATE project
-SET name = 'Project C'
-WHERE id = 3;
-UPDATE project
-SET name = 'Project D'
-WHERE id = 4;
-UPDATE project
-SET name = 'Project E'
-WHERE id = 5;
-UPDATE project
-SET name = 'Project F'
-WHERE id = 6;
-UPDATE project
-SET name = 'Project G'
-WHERE id = 7;
-UPDATE project
-SET name = 'Project H'
-WHERE id = 8;
-UPDATE project
-SET name = 'Project I'
-WHERE id = 9;
-UPDATE project
-SET name = 'Project J'
-WHERE id = 10;
 
 /*table project_worker*/
 CREATE TABLE project_worker(
--- print_project_prices.sql
+++ print_project_prices.sql
@@ -1,8 +1,8 @@
 /*projects price*/
-SELECT p.name AS name,
+SELECT p.id, p.name AS name,
 SUM(w.salary * DATEDIFF('MONTH', p.start_date, p.finish_date)) AS price
 FROM project p
 JOIN project_worker pw ON p.id = pw.project_id
 JOIN worker w ON pw.worker_id = w.id
-GROUP BY p.name
+GROUP BY p.id, p.name
 ORDER BY price DESC;
